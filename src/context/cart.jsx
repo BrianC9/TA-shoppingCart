1336d7f73441d773d8a472b4a76ea88e91f20d67
Bryan Octavio <100432689@alumnos.uc3m.es>
2023-03-24 21:58:34 +0100

    todo: refactor cart context

--- src/context/cart.jsx
+++ src/context/cart.jsx
@@ -5,8 +5,17 @@ export const CartContext = createContext()
 export function CartContextProvider ({ children }) {
   const [cart, setCart] = useState([])
 
+  const addToCart = product => {}
+  const removeFromCart = product => {}
+  const clearCart = () => {
+    if (cart.length === 0) return
+    const uSure = window.confirm('Clear cart?')
+    if (!uSure) return
+    setCart([])
+  }
+  const modifyQuantity = product => {}
   return (
-    <CartContext.Provider value={{ cart, setCart }}>
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart, modifyQuantity }}>
       {children}
     </CartContext.Provider>
   )
