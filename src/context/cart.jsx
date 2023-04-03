eeffdb1ae51448232326d168046cd251c8c62d66
Bryan Cusme <bryan.cusme@devoteam.com>
2023-04-03 15:21:00 +0200

    refactor: useReducer

--- src/context/cart.jsx
+++ src/context/cart.jsx
@@ -1,10 +1,21 @@
-import { createContext, useState } from 'react'
-
+import { createContext, useReducer } from 'react'
+import { ACTION_TYPES, cartReducer, INITIAL_CART_STATE } from '../reducers/cartReducer'
 export const CartContext = createContext()
 
 export function CartContextProvider ({ children }) {
-  const [cart, setCart] = useState([])
+  const [cart, dispatch] = useReducer(cartReducer, INITIAL_CART_STATE)
+  const addToCart = (product) => dispatch({
+    type: ACTION_TYPES.ADD_TO_CART,
+    payload: product
+  })
+  const removeFromCart = product => dispatch({
+    type: ACTION_TYPES.DELETE_FORM_CART,
+    payload: product
+  })
 
+  const clearCart = () => dispatch({
+    type: ACTION_TYPES.CLEAR_CART
+  })
   return (
     <CartContext.Provider
       value={{ cart, addToCart, removeFromCart, clearCart }}
--- src/reducers/cartReducer.js
+++ src/reducers/cartReducer.js
@@ -1,37 +1,28 @@
 export const INITIAL_CART_STATE = []
 
-const ACTION_TYPES = {
+export const ACTION_TYPES = {
   ADD_TO_CART: 'ADD_TO_CART',
   DELETE_FORM_CART: 'DELETE_FORM_CART',
   CLEAR_CART: 'CLEAR_CART'
 }
 
-export const carReducer = (state, action) => {
+export const cartReducer = (state, action) => {
   const { type, payload } = action
   switch (type) {
-    case ACTION_TYPES.ADD_TO_CART:
-      if (state.some((product) => product.id === payload.id)) {
-        const updatedCart = state.map((product) => {
-          if (product.id === payload.id) {
-            product.quantity += 1
-          }
-          return product
-        })
-        return updatedCart
+    case ACTION_TYPES.ADD_TO_CART:{
+      const productInCartIndex = state.findIndex(item => item.id === payload.id)
+      if (productInCartIndex >= 0) {
+        const newState = structuredClone(state)
+        newState[productInCartIndex].quantity += 1
+        return newState
       } else {
-        return [...state, { ...action.payload, quantity: 1 }]
-      }
+        return [...state, { ...payload, quantity: 1 }]
+      } }
+
     case ACTION_TYPES.DELETE_FORM_CART:
       return state.filter((itemCart) => itemCart.id !== payload.id)
     case ACTION_TYPES.CLEAR_CART:{
-      if (state.length === 0) return
-
-      const uSure = window.confirm('Clear cart?')
-
-      if (!uSure) return
       return INITIAL_CART_STATE }
-    default: {
-      throw Error('Unknown action: ' + type)
-    }
   }
+  return state
 }
